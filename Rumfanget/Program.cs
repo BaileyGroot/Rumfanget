7bd9c4f69bfe39dd4362d14a9bc1e170d1141010
Grootrix <grootrix@gmail.com>
2021-08-09 12:24:47 +0200

    Add project files.

---
+++ Rumfanget/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Rumfanget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Rumfanget
+
+
+            // H = Højde
+
+            Console.WriteLine("Højde: ");
+
+            double h = double.Parse(Console.ReadLine());
+
+            // H = Bredde
+
+            Console.WriteLine("Bredde: ");
+
+            double b = double.Parse(Console.ReadLine());
+
+            // H = Længde
+
+            Console.WriteLine("Længde: ");
+
+            double l = double.Parse(Console.ReadLine());
+
+
+
+            // Tager de tre tal og ganger dem.
+
+            double v = h * b * l;
+
+
+
+            Console.WriteLine("Rumfanget er: " + v);
+
+
+
+            // Prevens the program from shutting down.
+            Console.ReadKey();
+        }
+    }
+}
